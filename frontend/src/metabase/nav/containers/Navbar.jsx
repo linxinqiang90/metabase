c5689d397c6c27b835f86c67d9fede5250e826f9
linxinqiang <linxinqiang@git.xrjiot.cn>
2018-09-14 18:52:52 +0800

    init

--- frontend/src/metabase/nav/containers/Navbar.jsx
+++ frontend/src/metabase/nav/containers/Navbar.jsx
@@ -296,7 +296,14 @@ export default class Navbar extends Component {
               <Button medium>{t`Ask a question`}</Button>
             </Link>
           )}
-          <Button medium>{t`Share List`}</Button>
+          <Link
+            to="/public/share"
+            mx={2}
+            target="_blank"
+            className="hide sm-show"
+          >
+            <Button medium>{t`Share List`}</Button>
+          </Link>
           <EntityMenu
             tooltip={t`Create`}
             className="hide sm-show"
